c7d57100f5ede737b3994e4ffe393022ffdcbcf8
Andrew Porritt <andrew.porritt@icloud.com>
2019-01-15 22:08:14 +0000

    challenges passed

--- byotest.py
+++ byotest.py
@@ -1,13 +1,16 @@
-def number_of_evens(numbers):
-    return 0
-
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
-
-test_are_equal(number_of_evens([1,2,3,4,5]),2)
 
 def test_not_equal(a,b):
     assert a!=b, "Did not expect {0} but got {1}".format(a,b)
 
 def test_is_in(collection, item):
     assert item in collection, "{0} does not contain {1}".format(collection, item)
+    
+def test_not_in(collection, item):
+    assert item not in collection, "{0} is contained in {1}".format(item, collection)
+    
+def test_is_between(lower,actual,upper):
+    assert lower <= actual <= upper, "{0} is not between {1} and {2}".format(actual,lower,upper)
+
+print("All Tests Passed")
